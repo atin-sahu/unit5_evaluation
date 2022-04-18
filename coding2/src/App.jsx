0a8070dcccfa2000c4d83ff9079a849999e1afed
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-04-18 16:09:13 +0530

    coding 2 done

---
+++ coding2/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { AddStudent } from "./components/AddStudent";
+import { ShowStudents } from "./components/ShowStudents";
+
+function App() {
+  const [state,setState] = useState("show")
+
+  const handleChange = ()=>{
+    if(state == "add"){
+      setState("show")
+    }else if(state == "show"){
+      setState("add");
+    }
+  }
+  return (
+    <div className="App">
+      <button className="togglebtn" onClick={handleChange}>{state==="show" ? "Add Data" : "Show Data"}</button>
+      {/* Show either  AddStudent component or ShowStudents dependeing on the above button click  */}
+      {/* make sure the table is shown initially, do not show form initially */}
+      {state ==="show" ? <ShowStudents/> :<AddStudent/>}
+      {/* make sure to show either of them do not both together */}
+    </div>
+  );
+}
+
+export default App;
